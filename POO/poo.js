fa1938660e7e1f5076f6e6d0dedd2ab0d69b7d30
yasmany <yasmanys88@gmail.com>
2021-02-17 10:56:22 -0300

    Herencia y polimorfismo

--- POO/poo.js
+++ POO/poo.js
@@ -1,16 +1,25 @@
-class animal {
+
+class Animal {
     constructor(especie, edad, color) {
         this.especie = especie;
         this.edad = edad;
         this.color = color;
     }
-
     verinfo() {
         document.write(`Soy un ${this.especie}, tengo ${this.edad} años
-        y soy de color ${this.color} <br>`)
+        y soy de color ${this.color} <br>`);
     }
+};
+class Perro extends Animal {
+    constructor(especie, edad, color, raza) {
+        super(especie, edad, color);
+        this.raza = raza;
+    }
+    ladrar() {
+        alert(`Esta Ladrando el ${this.raza}`);
+    }
+};
 
-}
-
-const perro = new animal("Perro", 5, "negro");
+const perro = new Perro("Perro", 5, "negro", "Chiguagua");
 perro.verinfo();
+perro.ladrar();
